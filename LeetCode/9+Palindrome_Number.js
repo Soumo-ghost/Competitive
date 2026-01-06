3325e88ce1fb58e428981f36502085dca8c16b9a
Soumo-ghost <chakrabortypremanshu@gmail.com>
2026-01-06 18:35:15 +0530

    Hi

---
+++ LeetCode/9+Palindrome_Number.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    let ans;
+    const a = Array.from(String(x), Number)
+    if (a.length % 2 === 0) {
+        for (let i = 0; i < (a.length / 2); i++) {
+            if (a[i] === a[((a.length - 1) - i)]) {
+                ans = true;
+            }
+            else {
+                ans = false;
+                break;
+            }
+        }
+    }
+    else if(x<10 && x>=0){
+        ans=true;
+    }
+    else {
+        for (let i = 0; i < (a.length / 2) - 1; i++) {
+            if (a[i] === a[((a.length - 1) - i)]) {
+                ans = true;
+            }
+            else {
+                ans = false;
+                break;
+
+            }
+        }
+    }
+
+        return ans;
+    };
